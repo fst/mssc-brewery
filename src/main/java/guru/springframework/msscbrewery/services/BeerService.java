24ea486c22d3c4cfbf86a1a95133dc95857f5cd6
Florian Stoller <220617+fst@users.noreply.github.com>
2021-04-01 16:13:31 +0200

    state after lesson 30

--- src/main/java/guru/springframework/msscbrewery/services/BeerService.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerService.java
@@ -1,14 +1,13 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.context.annotation.Bean;
 
 import java.util.UUID;
 
-/**
- * Created by jt on 2019-04-20.
- */
+
 public interface BeerService {
     BeerDto getBeerById(UUID beerId);
 
-    //hello world
+    BeerDto saveNewBeer(BeerDto beerDto);
 }
